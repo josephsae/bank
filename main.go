f0fe95a49431496ed07b287af3879682d8f878fd
Joseph Caicedo Sáenz <joseph.caice08@gmail.com>
2023-03-11 19:58:53 -0500

    feat: create restful http api with gin
    - install gin
    - create server
    - create, update and get routes/controller for accounts

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/josephsae/bank/api"
+	db "github.com/josephsae/bank/db/sqlc"
+	"github.com/josephsae/bank/utils"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
